550a722b8112a85b46f3dedd45d149b897444f50
Vision <74459856+Vision-zz@users.noreply.github.com>
2022-09-29 15:45:34 +0530

    Add a staic block in Main class before the main method to create a university. Plus displaying my laziness :)

--- Associations/Main.java
+++ Associations/Main.java
@@ -1,6 +1,17 @@
 public class Main {
 
+    static {
+        University university = University.getInstance();
+        String[] deptStrings = { "Mechanical", "Aero", "Automobile", "Mechatronics", "Biotech" };
+        for(String dept : deptStrings) {
+            university.addDepartment(new Department(dept));
+        }
+    }
+
     public static void main(String[] args) {
+
+        // Implement all features like adding depts, professors, friends of professors and stuff.
+        // Im just lazy, create pull request and I might merge it. Thanks :)
 
     }
 }
