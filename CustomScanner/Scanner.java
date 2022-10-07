6ac3074b1672430f7a258ef324610aa4472e20f5
Vision <74459856+Vision-zz@users.noreply.github.com>
2022-10-07 11:43:45 +0530

    Refactor exceptions

--- CustomScanner/Scanner.java
+++ CustomScanner/Scanner.java
@@ -12,50 +12,50 @@ public class Scanner {
         return System.console().readLine(prompt + "\n", args);
     }
 
-    public static int getInt() throws Exception {
+    public static int getInt() throws NumberFormatException {
         return getInt(null);
     }
 
-    public static int getInt(String prompt, Object... args) throws Exception {
+    public static int getInt(String prompt, Object... args) throws NumberFormatException {
         String input = System.console().readLine(prompt + "\n", args);
         return Integer.parseInt(input);
     }
 
-    public static double getDouble() throws Exception {
+    public static double getDouble() throws NumberFormatException {
         return getDouble(null);
     }
 
-    public static double getDouble(String prompt, Object... args) throws Exception {
+    public static double getDouble(String prompt, Object... args) throws NumberFormatException {
         String input = System.console().readLine(prompt + "\n", args);
         return Double.parseDouble(input);
     }
 
-    public static float getFloat() throws Exception {
+    public static float getFloat() throws NumberFormatException {
         return getFloat(null);
     }
 
-    public static float getFloat(String prompt, Object... args) throws Exception {
+    public static float getFloat(String prompt, Object... args) throws NumberFormatException {
         String input = System.console().readLine(prompt + "\n", args);
         return Float.parseFloat(input);
     }
 
-    public static long getLong() throws Exception {
+    public static long getLong() throws NumberFormatException {
         return getLong(null);
     }
 
-    public static long getLong(String prompt, Object... args) throws Exception {
+    public static long getLong(String prompt, Object... args) throws NumberFormatException {
         String input = System.console().readLine(prompt, args);
         return Long.parseLong(input);
     }
 
-    public static char getChar() throws Exception {
+    public static char getChar() throws NumberFormatException {
         return getChar(null);
     }
 
-    public static char getChar(String prompt, Object... args) throws Exception {
+    public static char getChar(String prompt, Object... args) throws NumberFormatException {
         String input = System.console().readLine(prompt, args);
         if (input.length() > 1)
-            throw new Exception("Character length is out of bounds");
+            throw new NumberFormatException("Character length is out of bounds");
         return input.toCharArray()[0];
     }
 
